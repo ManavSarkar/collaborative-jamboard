15c87298d661056662c18591031ee95061e988e4
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-06-07 21:41:29 +0530

    removing pre and adding secret key

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -15,18 +15,14 @@ const user = new mongoose.Schema({
     required: true,
   },
 })
-user.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 12)
-  }
-  next()
-})
+
 user.methods.generateAuthToken = async function () {
   try {
-    // TODO: Change the secret key to something more secure
-    let token = jwt.sign({ _id: this._id }, 'SECRET_KEY')
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY)
     return token
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
 }
 const User = mongoose.model('USER', user)
 
