638a990cd20ccdc5646b2b91e6b89017fb900047
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-06-07 21:42:05 +0530

    update user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -19,13 +19,15 @@ router.post('/register', async (req, res) => {
   if (existingUser !== null) {
     res.status(401).json({ success: false, message: 'User exists' })
   } else {
-    user.save(function (err, user) {
-      if (err) {
-        res.status(401).json({ success: false, message: 'Please try again' })
-      } else {
+    user.password = await bcrypt.hash(user.password, 12)
+    user
+      .save()
+      .then(() => {
         res.status(200).json({ success: true, message: 'User created' })
-      }
-    })
+      })
+      .catch((error) => {
+        res.status(401).json({ success: false, message: 'Please try again' })
+      })
   }
 })
 
@@ -36,22 +38,30 @@ router.post('/logout', (req, res) => {
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body
-  User.findOne({ email: email.toLowerCase() }, async function (err, user) {
-    if (user === null) {
+  try {
+    const user = await User.findOne({ email: email.toLowerCase() })
+
+    if (!user) {
       return res.json({ success: false })
-    } else {
-      const match = await bcrypt.compare(password, user.password)
-      if (!match) {
-        res.json({ success: false })
-      } else {
-        var token = await user.generateAuthToken()
-        res.cookie(process.env.AUTH_COOKIE, token, {
-          expires: new Date(Date.now() + 2592000000),
-          httpOnly: true,
-        })
-        res.json({ success: true })
-      }
     }
-  })
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      console.log('Password does not match')
+      return res.json({ success: false })
+    }
+
+    const token = await user.generateAuthToken()
+    res.cookie(process.env.AUTH_COOKIE, token, {
+      expires: new Date(Date.now() + 2592000000),
+      httpOnly: true,
+    })
+
+    res.json({ success: true })
+  } catch (error) {
+    // Handle any potential errors
+    console.error(error)
+    res.status(500).json({ success: false, error: 'Internal Server Error' })
+  }
 })
 module.exports = router
