433b2109cd981afac7b74bdd9124289a04fd5fdb
Manav Sarkar <manavsarkar07@gmail.com>
2023-06-24 17:17:54 +0530

    cchat

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -3,7 +3,7 @@ import '../styles/main.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import whiteBoard from './whiteBoard'
 
-import io from 'socket.io-client';
+import io from 'socket.io-client'
 
 import {
   faCaretDown,
@@ -19,154 +19,145 @@ import {
   faUndo,
   faUpload,
   faDownload,
-  faShare
+  faShare,
 } from '@fortawesome/free-solid-svg-icons'
 
-var colorPen = 'black', zoomval = 100;
+var colorPen = 'black',
+  zoomval = 100
 
 function Board() {
-  const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
-  const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
-  const [isDropdownOpen3, setIsDropdownOpen3] = useState(false);
-  const [isDropdownOpen4, setIsDropdownOpen4] = useState(false);
+  const [isDropdownOpen1, setIsDropdownOpen1] = useState(false)
+  const [isDropdownOpen2, setIsDropdownOpen2] = useState(false)
+  const [isDropdownOpen3, setIsDropdownOpen3] = useState(false)
+  const [isDropdownOpen4, setIsDropdownOpen4] = useState(false)
 
-  const [selectedColor1, setSelectedColor1] = useState('black');
+  const [selectedColor1, setSelectedColor1] = useState('black')
   // const [selectedColor2, setSelectedColor2] = useState('black');
 
-  const [zoomLevel, setZoomLevel] = useState(100);
-  const [zoomLevel1, setZoomLevel1] = useState(100);
-  const [zoomLevel2, setZoomLevel2] = useState(100);
-  const [zoomLevel3, setZoomLevel3] = useState(100);
+  const [zoomLevel, setZoomLevel] = useState(100)
+  const [zoomLevel1, setZoomLevel1] = useState(100)
+  const [zoomLevel2, setZoomLevel2] = useState(100)
+  const [zoomLevel3, setZoomLevel3] = useState(100)
 
   const notoggleDropdown = () => {
-    setIsDropdownOpen1(false);
-    setIsDropdownOpen2(false);
-    setIsDropdownOpen3(false);
+    setIsDropdownOpen1(false)
+    setIsDropdownOpen2(false)
+    setIsDropdownOpen3(false)
   }
 
   const toggleDropdown1 = () => {
-    colorPen = 'black';
-    zoomval = 100;
-    setIsDropdownOpen1(!isDropdownOpen1);
-    setIsDropdownOpen2(false);
-    setIsDropdownOpen3(false);
-  };
+    colorPen = 'black'
+    zoomval = 100
+    setIsDropdownOpen1(!isDropdownOpen1)
+    setIsDropdownOpen2(false)
+    setIsDropdownOpen3(false)
+  }
   const toggleDropdown2 = () => {
-    colorPen = 'white';
+    colorPen = 'white'
     // zoomval = 500;
-    setIsDropdownOpen2(!isDropdownOpen2);
-    setIsDropdownOpen1(false);
-    setIsDropdownOpen3(false);
-  };
+    setIsDropdownOpen2(!isDropdownOpen2)
+    setIsDropdownOpen1(false)
+    setIsDropdownOpen3(false)
+  }
   const toggleDropdown3 = () => {
-    setIsDropdownOpen3(!isDropdownOpen3);
-    setIsDropdownOpen1(false);
-    setIsDropdownOpen2(false);
-  };
+    setIsDropdownOpen3(!isDropdownOpen3)
+    setIsDropdownOpen1(false)
+    setIsDropdownOpen2(false)
+  }
   const toggleDropdown4 = () => {
-    setIsDropdownOpen4(!isDropdownOpen4);
-  };
-
-
-
-  const handleZoomChange1 = (event) => {
-    zoomval = parseInt(event.target.value);
-
-    const newZoomLevel = parseInt(event.target.value);
-    setZoomLevel1(newZoomLevel);
-  };
-  const handleZoomChange2 = (event) => {
-    
-    zoomval = parseInt(event.target.value) + 5000;
-
-    const newZoomLevel = parseInt(event.target.value);
-    setZoomLevel2(newZoomLevel);
-  };
-  const handleZoomChange3 = (event) => {
-    const newZoomLevel = parseInt(event.target.value);
-    setZoomLevel3(newZoomLevel);
-  };
-
-  const handleZoomIn = (zoom) => {
-    setZoomLevel(zoom);
-    setIsDropdownOpen4(false); // Close the dropdown after selecting zoom option
-  };
-
-
-
-  const handleColorChange1 = (color) => {
-    colorPen = color;
-    setSelectedColor1(color);
-    setIsDropdownOpen1(false);
-  };
-
-
-
-  const deleteBorad = () => {
-    setIsDropdownOpen1(false);
-    setIsDropdownOpen2(false);
-    setIsDropdownOpen3(false);
-    setIsDropdownOpen4(false);
-    setSelectedColor1('black');
-    setZoomLevel(100);
-    setZoomLevel1(100);
-    setZoomLevel2(100);
-    setZoomLevel3(100);
+    setIsDropdownOpen4(!isDropdownOpen4)
   }
 
+  const handleZoomChange1 = (event) => {
+    zoomval = parseInt(event.target.value)
 
+    const newZoomLevel = parseInt(event.target.value)
+    setZoomLevel1(newZoomLevel)
+  }
+  const handleZoomChange2 = (event) => {
+    zoomval = parseInt(event.target.value) + 5000
 
+    const newZoomLevel = parseInt(event.target.value)
+    setZoomLevel2(newZoomLevel)
+  }
+  const handleZoomChange3 = (event) => {
+    const newZoomLevel = parseInt(event.target.value)
+    setZoomLevel3(newZoomLevel)
+  }
 
-  const canvasRef = useRef(null);
-  const colorsRef = useRef(null);
-  const socketRef = useRef();
-  const bsize = useRef(null);
+  const handleZoomIn = (zoom) => {
+    setZoomLevel(zoom)
+    setIsDropdownOpen4(false) // Close the dropdown after selecting zoom option
+  }
+
+  const handleColorChange1 = (color) => {
+    colorPen = color
+    setSelectedColor1(color)
+    setIsDropdownOpen1(false)
+  }
+
+  const deleteBorad = () => {
+    setIsDropdownOpen1(false)
+    setIsDropdownOpen2(false)
+    setIsDropdownOpen3(false)
+    setIsDropdownOpen4(false)
+    setSelectedColor1('black')
+    setZoomLevel(100)
+    setZoomLevel1(100)
+    setZoomLevel2(100)
+    setZoomLevel3(100)
+  }
+
+  const canvasRef = useRef(null)
+  const colorsRef = useRef(null)
+  const socketRef = useRef()
+  const bsize = useRef(null)
 
   useEffect(() => {
-
     // --------------- getContext() method returns a drawing context on the canvas-----
 
-    const canvas = canvasRef.current;
-    const test = colorsRef.current;
-    const context = canvas.getContext('2d');
-
+    const canvas = canvasRef.current
+    const test = colorsRef.current
+    const context = canvas.getContext('2d')
 
     // ----------------------- Colors --------------------------------------------------
 
-    const colors = document.getElementsByClassName('color');
-    console.log(colors, 'the colors');
-    console.log(test);
+    const colors = document.getElementsByClassName('color')
+    console.log(colors, 'the colors')
+    console.log(test)
     // set the current color
     const current = {
       color: 'black',
-    };
+    }
 
     // helper that will update the current color
     const onColorUpdate = (e) => {
-      current.color = e.target.className.split(' ')[1];
-    };
+      current.color = e.target.className.split(' ')[1]
+    }
 
     // loop through the color elements and add the click event listeners
     for (let i = 0; i < colors.length; i++) {
-      colors[i].addEventListener('click', onColorUpdate, false);
+      colors[i].addEventListener('click', onColorUpdate, false)
     }
-    let drawing = false;
+    let drawing = false
 
     // ------------------------------- create the drawing ----------------------------
-    console.log(colorPen);
+    console.log(colorPen)
     const drawLine = (x0, y0, x1, y1, color, emit) => {
-      context.beginPath();
+      context.beginPath()
 
-      context.moveTo(x0, y0);
-      context.lineTo(x1, y1);
-      context.strokeStyle = colorPen;
-      context.lineWidth = (2 * (zoomval / 100));
-      context.stroke();
-      context.closePath();
+      context.moveTo(x0, y0)
+      context.lineTo(x1, y1)
+      context.strokeStyle = colorPen
+      context.lineWidth = 2 * (zoomval / 100)
+      context.stroke()
+      context.closePath()
 
-      if (!emit) { return; }
-      const w = canvas.width;
-      const h = canvas.height;
+      if (!emit) {
+        return
+      }
+      const w = canvas.width
+      const h = canvas.height
 
       socketRef.current.emit('drawing', {
         x0: x0 / w,
@@ -174,50 +165,68 @@ function Board() {
         x1: x1 / w,
         y1: y1 / h,
         color,
-      });
-    };
+      })
+    }
 
     // ---------------- mouse movement --------------------------------------
 
     const onMouseDown = (e) => {
-      drawing = true;
-      current.x = e.clientX || e.touches[0].clientX;
-      current.y = e.clientY || e.touches[0].clientY;
-    };
+      drawing = true
+      current.x = e.clientX || e.touches[0].clientX
+      current.y = e.clientY || e.touches[0].clientY
+    }
 
     const onMouseMove = (e) => {
-      if (!drawing) { return; }
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
-      current.x = e.clientX || e.touches[0].clientX;
-      current.y = e.clientY || e.touches[0].clientY;
-    };
+      if (!drawing) {
+        return
+      }
+      drawLine(
+        current.x,
+        current.y,
+        e.clientX || e.touches[0].clientX,
+        e.clientY || e.touches[0].clientY,
+        current.color,
+        true,
+      )
+      current.x = e.clientX || e.touches[0].clientX
+      current.y = e.clientY || e.touches[0].clientY
+    }
 
     const onMouseUp = (e) => {
-      if (!drawing) { return; }
-      drawing = false;
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
-    };
+      if (!drawing) {
+        return
+      }
+      drawing = false
+      drawLine(
+        current.x,
+        current.y,
+        e.clientX || e.touches[0].clientX,
+        e.clientY || e.touches[0].clientY,
+        current.color,
+        true,
+      )
+    }
 
     // ----------- limit the number of events per second -----------------------
 
     const throttle = (callback, delay) => {
-      let previousCall = new Date().getTime();
+      let previousCall = new Date().getTime()
       return function () {
-        const time = new Date().getTime();
+        const time = new Date().getTime()
 
-        if ((time - previousCall) >= delay) {
-          previousCall = time;
-          callback.apply(null, arguments);
+        if (time - previousCall >= delay) {
+          previousCall = time
+          callback.apply(null, arguments)
         }
-      };
-    };
+      }
+    }
 
     // -----------------add event listeners to our canvas ----------------------
 
-    canvas.addEventListener('mousedown', onMouseDown, false);
-    canvas.addEventListener('mouseup', onMouseUp, false);
-    canvas.addEventListener('mouseout', onMouseUp, false);
-    canvas.addEventListener('mousemove', throttle(onMouseMove, 5), false);
+    canvas.addEventListener('mousedown', onMouseDown, false)
+    canvas.addEventListener('mouseup', onMouseUp, false)
+    canvas.addEventListener('mouseout', onMouseUp, false)
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 5), false)
 
     // Touch support for mobile devices
     // canvas.addEventListener('touchstart', onMouseDown, false);
@@ -228,33 +237,37 @@ function Board() {
     // -------------- make the canvas fill its parent component -----------------
 
     const onResize = () => {
-      canvas.width = bsize.current.offsetWidth;
-      canvas.height = bsize.current.offsetHeight;
-    };
+      canvas.width = bsize.current.offsetWidth
+      canvas.height = bsize.current.offsetHeight
+    }
 
-    window.addEventListener('resize', onResize, false);
-    onResize();
+    window.addEventListener('resize', onResize, false)
+    onResize()
 
     // ----------------------- socket.io connection ----------------------------
     const onDrawingEvent = (data) => {
-      const w = canvas.width;
-      const h = canvas.height;
-      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+      const w = canvas.width
+      const h = canvas.height
+      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color)
     }
 
-    socketRef.current = io.connect('/');
-    socketRef.current.on('drawing', onDrawingEvent);
-  }, []);
-
+    socketRef.current = io.connect('http://localhost:8080')
+    socketRef.current.on('drawing', onDrawingEvent)
+  }, [])
 
   return (
-    <div className=' w-full bg-slate-50 min-h-screen mx-2 col-span-2 text-black'>
-
-      <div ref={bsize} id='container' className="my-2 h-5/6 border-black border-2 mx" >
-        <canvas 
-        style={{ zoom: `${zoomLevel}%`}} 
-        id='canvas' ref={canvasRef} 
-        className="whiteboard" />
+    <div className=" w-full bg-slate-50 min-h-screen mx-2 col-span-2 text-black">
+      <div
+        ref={bsize}
+        id="container"
+        className="my-2 h-5/6 border-black border-2 mx"
+      >
+        <canvas
+          style={{ zoom: `${zoomLevel}%` }}
+          id="canvas"
+          ref={canvasRef}
+          className="whiteboard"
+        />
 
         <div className="tools">
           <div className="un-redo">
@@ -268,21 +281,28 @@ function Board() {
           </div>
 
           <div className="zoomin">
-            <button onClick={toggleDropdown4} type="button" className="pen-tools">
+            <button
+              onClick={toggleDropdown4}
+              type="button"
+              className="pen-tools"
+            >
               <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
             </button>
-            <button onClick={toggleDropdown4} type="button" className="pen-tools">
+            <button
+              onClick={toggleDropdown4}
+              type="button"
+              className="pen-tools"
+            >
               <FontAwesomeIcon icon={faCaretDown} />
             </button>
             {isDropdownOpen4 && (
-              <div className='dropdown-content zoom'>
+              <div className="dropdown-content zoom">
                 <button onClick={() => handleZoomIn(75)}>75%</button>
                 <button onClick={() => handleZoomIn(100)}>100%</button>
                 <button onClick={() => handleZoomIn(150)}>150%</button>
                 <button onClick={() => handleZoomIn(200)}>200%</button>
               </div>
             )}
-
           </div>
 
           <div className="pages">
@@ -300,12 +320,12 @@ function Board() {
               onClick={toggleDropdown1}
               style={{ zoom: `${zoomLevel1}%`, color: selectedColor1 }}
               type="button"
-              className="pen-tools">
-
+              className="pen-tools"
+            >
               <FontAwesomeIcon icon={faPencil} />
             </button>
             {isDropdownOpen1 && (
-              <div className='dropdown-content pen'>
+              <div className="dropdown-content pen">
                 <input
                   type="range"
                   min="50"
@@ -313,26 +333,44 @@ function Board() {
                   value={zoomLevel1}
                   onChange={handleZoomChange1}
                 />
-                <div className='colors'>
-                  <button onClick={() => handleColorChange1('black')} style={{ backgroundColor: 'black' }}></button>
-                  <button onClick={() => handleColorChange1('#f5314b')} style={{ backgroundColor: '#f5314b' }}></button>
-                  <button onClick={() => handleColorChange1('cyan')} style={{ backgroundColor: 'cyan' }}></button>
-                  <button onClick={() => handleColorChange1('#31f58c')} style={{ backgroundColor: '#31f58c' }}></button>
-                  <button onClick={() => handleColorChange1('#f5c131')} style={{ backgroundColor: '#f5c131' }}></button>
-                  <button onClick={() => handleColorChange1('#3e31f5')} style={{ backgroundColor: '#3e31f5' }}></button>
+                <div className="colors">
+                  <button
+                    onClick={() => handleColorChange1('black')}
+                    style={{ backgroundColor: 'black' }}
+                  ></button>
+                  <button
+                    onClick={() => handleColorChange1('#f5314b')}
+                    style={{ backgroundColor: '#f5314b' }}
+                  ></button>
+                  <button
+                    onClick={() => handleColorChange1('cyan')}
+                    style={{ backgroundColor: 'cyan' }}
+                  ></button>
+                  <button
+                    onClick={() => handleColorChange1('#31f58c')}
+                    style={{ backgroundColor: '#31f58c' }}
+                  ></button>
+                  <button
+                    onClick={() => handleColorChange1('#f5c131')}
+                    style={{ backgroundColor: '#f5c131' }}
+                  ></button>
+                  <button
+                    onClick={() => handleColorChange1('#3e31f5')}
+                    style={{ backgroundColor: '#3e31f5' }}
+                  ></button>
                 </div>
               </div>
-
             )}
             <button
               onClick={toggleDropdown2}
               style={{ zoom: `${zoomLevel2}%` }}
               type="button"
-              className="pen-tools">
+              className="pen-tools"
+            >
               <FontAwesomeIcon icon={faEraser} />
             </button>
             {isDropdownOpen2 && (
-              <div className='dropdown-content eraser'>
+              <div className="dropdown-content eraser">
                 <input
                   type="range"
                   min="50"
@@ -346,12 +384,12 @@ function Board() {
               onClick={toggleDropdown3}
               style={{ zoom: `${zoomLevel3}%` }}
               type="button"
-              className="pen-tools">
-
+              className="pen-tools"
+            >
               <FontAwesomeIcon icon={faShapes} />
             </button>
             {isDropdownOpen3 && (
-              <div className='dropdown-content shapes'>
+              <div className="dropdown-content shapes">
                 <input
                   type="range"
                   min="50"
@@ -395,15 +433,17 @@ function Board() {
         >
       </canvas> */}
 
-
       <div className="flex justify-center items-center my-4">
         <button className="btn btn-outline btn-error mx-4 w-36">Leave</button>
-        <button className="btn btn-outline btn-info mx-4 w-40">Download <FontAwesomeIcon icon={faDownload} className='mx-1' /></button>
-        <button className="btn btn-outline btn-success mx-4 w-36">Share <FontAwesomeIcon icon={faShare} className='mx-1' /></button>
-
+        <button className="btn btn-outline btn-info mx-4 w-40">
+          Download <FontAwesomeIcon icon={faDownload} className="mx-1" />
+        </button>
+        <button className="btn btn-outline btn-success mx-4 w-36">
+          Share <FontAwesomeIcon icon={faShare} className="mx-1" />
+        </button>
       </div>
     </div>
   )
 }
 
-export default Board;
+export default Board
--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState, useRef } from 'react'
+import { useMemo } from 'react'
+
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:8080')
 
 function Chat() {
+  const [currentMessage, setCurrentMessage] = useState('')
+  const [messageList, setMessageList] = useState([])
+  const sendMessage = async () => {
+    if (currentMessage !== '') {
+      const messageData = {
+        author: socket.id.toString(),
+        message: currentMessage,
+        date:
+          new Date(Date.now()).getHours() +
+          ':' +
+          new Date(Date.now()).getMinutes() +
+          ':' +
+          new Date(Date.now()).getSeconds(),
+      }
+      await socket.emit('send_message', messageData)
+      setMessageList((list) => [...list, messageData])
+      setCurrentMessage('')
+    }
+  }
+
+  useMemo(() => {
+    socket.on('receive_message', (data) => {
+      setMessageList((list) => [...list, data])
+    })
+  }, [socket])
+
   return (
-    <div>
-      hello
+    <div className="max-w-lg mx-auto " style={{ height: '83%' }}>
+      <div className="bg-white rounded-lg shadow">
+        <div className="px-6 py-4">
+          <h2 className="text-lg font-semibold text-gray-800">Chat</h2>
+        </div>
+        <div className="px-6 py-4">
+          <ul className="space-y-4">
+            {/* Message List */}
+            {messageList.map((message, index) => (
+              <li
+                className={
+                  'chat' +
+                  (message.author === socket.id ? 'chat-end' : ' chat-start')
+                }
+                key={index}
+              >
+                <div className="flex-shrink-0">
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src={message.author}
+                    alt={message.author}
+                  />
+                </div>
+                <div className="ml-4">
+                  <p className="text-sm font-medium text-gray-900">
+                    {message.author.name}
+                  </p>
+                  <p className="text-sm text-gray-700">{message.message}</p>
+                  <p className="text-xs text-gray-500">{message.date}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="px-6 py-4 bg-gray-100">
+          <div className="flex space-x-3">
+            <input
+              className="flex-grow border border-gray-300 rounded-md py-2 px-4"
+              type="text"
+              placeholder="Type a message..."
+              value={currentMessage}
+              onChange={(event) => setCurrentMessage(event.target.value)}
+              onKeyPress={(event) =>
+                event.key === 'Enter' ? sendMessage() : null
+              }
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+              onClick={sendMessage}
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
