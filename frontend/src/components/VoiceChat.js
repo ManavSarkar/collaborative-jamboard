e5cfebf13b409a324cb097fc7c406737584ea2fd
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-07-16 21:52:19 +0530

    Update VoiceChat.js

--- frontend/src/components/VoiceChat.js
+++ frontend/src/components/VoiceChat.js
@@ -31,6 +31,13 @@ const VoiceChat = ({ socket }) => {
 
     peer.on("call", (call) => {
       call.answer(myStreamRef.current);
+      call.on("stream", (remoteStream) => {
+        document.getElementsByTagName("audio")[count].srcObject = remoteStream;
+        document.getElementsByTagName("audio")[count].play();
+        document.getElementsByTagName("audio")[count].id = call.peer;
+        setCount((count) => count + 1);
+        console.log("count", count);
+      });
     });
     socket.on("connected-users", (users) => {
       console.log("connected-users", users);
@@ -47,13 +54,13 @@ const VoiceChat = ({ socket }) => {
     if (peerRef.current == null || peerRef.current.id === peerID) return;
     const call = peerRef.current.call(peerID, myStreamRef.current);
     if (call === undefined) return;
-    call.on("stream", (remoteStream) => {
-      document.getElementsByTagName("audio")[count].srcObject = remoteStream;
-      document.getElementsByTagName("audio")[count].play();
-      document.getElementsByTagName("audio")[count].id = peerID;
-      setCount((count) => count + 1);
-      console.log("count", count);
-    });
+    // call.on("stream", (remoteStream) => {
+    //   document.getElementsByTagName("audio")[count].srcObject = remoteStream;
+    //   document.getElementsByTagName("audio")[count].play();
+    //   document.getElementsByTagName("audio")[count].id = peerID;
+    //   setCount((count) => count + 1);
+    //   console.log("count", count);
+    // });
   };
   const handleMute = () => {
     myStreamRef.current.getAudioTracks()[0].enabled =
